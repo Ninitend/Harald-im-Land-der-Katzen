2bf9d3b72aa6218a01519f3ab345fd57fbca635a
Bosse <ninitend@gmail.com>
2024-11-26 14:54:40 +0100

    Collision shape of cat

--- script/cat.gd
+++ script/cat.gd
@@ -1,2 +1,16 @@
 extends CharacterBody2D
-class_name cat
+class_name Cat
+
+func _ready() -> void:
+	$Animation.play("idle")
+
+
+# ---------- Node Signals --------------------
+func _on_sight_range_body_entered(body: Node2D) -> void:
+	if body is Player:
+		$Animation.play('walk')
+
+
+func _on_sight_range_body_exited(body: Node2D) -> void:
+	if body is Player:
+		$Animation.play('idle')
