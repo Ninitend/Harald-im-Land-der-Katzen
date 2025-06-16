1f03f667f56807b6b4fb3c0498893285545b8f0a
Maxi <maxi1311@icloud.com>
2025-06-16 16:43:26 +0200

    Fix and cleanup textbox script

--- script/textbox.gd
+++ script/textbox.gd
@@ -5,72 +5,77 @@ extends CanvasLayer
 @onready var end_symbol = $TextboxContainer/MarginContainer/HBoxContainer/End
 @onready var content = $TextboxContainer/MarginContainer/HBoxContainer/Label
 
-var tween = create_tween()
-var char_read_rate = 0.05
-var current_state = State.Ready
-var text_queue = []
-
 enum State {
 	Ready,
 	Reading,
 	Finished
 }
 
+var tween = null
+var char_read_rate: float = 0.05
+var current_state: State = State.Ready
+var text_queue = []
 
 
 func _ready() -> void:
 	hide_texbox()
-	queue_text("1. Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
-	#queue_text("2. Irgendwas stimmt da nicht, oder?")
+	queue_text("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod.")
+	queue_text("Labore et dolore magna aliquyam erat, sed diam voluptua.")
+	queue_text("At vero eos et accusam et justo duo dolores et ea rebum.")
 
-func _process(delta: float) -> void:
+
+func _process(_delta: float) -> void:
 	match current_state:
 		State.Ready:
-			if !text_queue.is_empty():
+			if not text_queue.is_empty():
 				display_text()
 		State.Reading:
 			if Input.is_action_just_pressed("dialogue_enter"):
-				content.visible_ratio = 1.0
 				tween.kill()
-				end_symbol.text = "v"
-				change_state(State.Finished)
+				content.visible_ratio = 1.0
+				text_finished()
 		State.Finished:
-			 			if Input.is_action_just_pressed("dialogue_enter"):
- 				if text_queue.is_empty():
- 					change_state(State.Ready)
- 					hide_texbox()
- 				else:
- 				 display_text()
+			if Input.is_action_just_pressed("dialogue_enter"):
+				if text_queue.is_empty():
+					change_state(State.Ready)
+					hide_texbox()
+				else:
+					display_text()
 
 
-func queue_text(next_text):
-	text_queue.push_back(next_text)
+func queue_text(text: String) -> void:
+	text_queue.push_back(text)
 
-func hide_texbox():
+
+func hide_texbox() -> void:
 	content.text = ""
 	start_symbol.text = ""
 	end_symbol.text = ""
 	textboxContainer.hide()
 
-func show_textbox():
+
+func show_textbox() -> void:
 	start_symbol.text = "-"
 	textboxContainer.show()
 
-func display_text():
+
+func display_text() -> void:
+	change_state(State.Reading)
 	var next_text = text_queue.pop_front()
 	content.text = next_text
 	content.visible_ratio = 0.0
-	change_state(State.Reading)
 	show_textbox()
-	
+	tween = create_tween()
 	tween.tween_property(content, "visible_ratio", 1.0, len(next_text) * char_read_rate)
-	tween.finished.connect(_on_tween_finished)
-	
-func _on_tween_finished():
+	tween.tween_callback(text_finished)
+
+
+func text_finished() -> void:
 	end_symbol.text = "v"
 	change_state(State.Finished)
 
-func change_state(next_state):
+
+func change_state(next_state) -> void:
 	current_state = next_state
 	match current_state:
 		State.Ready:
